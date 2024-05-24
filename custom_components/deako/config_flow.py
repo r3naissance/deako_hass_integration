b17ee9c7dab62d67afb51d30bfecbfd38d8a4cf1
r3naissance <r3naissance@users.noreply.github.com>
2024-05-24 14:03:26 -0600

    Update config_flow.py

--- custom_components/deako/config_flow.py
+++ custom_components/deako/config_flow.py
@@ -1,97 +1,26 @@
-"""Adds config flow for Deako."""
-from homeassistant import config_entries
-from homeassistant.core import callback
-import voluptuous as vol
+"""Config flow for deako."""
 
-from .const import (
-    CONF_IP,
-    DOMAIN,
-    PLATFORMS,
-)
+from pydeako.discover import DeakoDiscoverer, DevicesNotFoundException
 
-class DeakoFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    """Config flow for Deako."""
+from homeassistant.components import zeroconf
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_entry_flow
 
-    VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
+from .const import DOMAIN, NAME
 
-    def __init__(self):
-        """Initialize."""
-        self._errors = {}
 
-    async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-        self._errors = {}
+async def _async_has_devices(hass: HomeAssistant) -> bool:
+    """Return if there are devices that can be discovered."""
+    _zc = await zeroconf.async_get_instance(hass)
+    discoverer = DeakoDiscoverer(_zc)
 
-        # Uncomment the next 2 lines if only a single instance of the integration is allowed:
-        # if self._async_current_entries():
-        #     return self.async_abort(reason="single_instance_allowed")
-
-        if user_input is not None:
-            valid = await self._test_credentials(
-                user_input[CONF_IP]
-            )
-            if valid:
-                return self.async_create_entry(
-                    title=user_input[CONF_IP], data=user_input
-                )
-            else:
-                self._errors["base"] = "auth"
-
-            return await self._show_config_form(user_input)
-
-        return await self._show_config_form(user_input)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return DeakoOptionsFlowHandler(config_entry)
-
-    async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
-        """Show the configuration form to edit location data."""
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_IP): str}
-            ),
-            errors=self._errors,
-        )
-
-    async def _test_credentials(self, ip):
-        """Return true if credentials is valid."""
+    try:
+        await discoverer.get_address()
+        # address exists, there's at least one device
         return True
 
+    except DevicesNotFoundException:
+        return False
 
-class DeakoOptionsFlowHandler(config_entries.OptionsFlow):
-    """Deako config flow options handler."""
 
-    def __init__(self, config_entry):
-        """Initialize HACS options flow."""
-        self.config_entry = config_entry
-        self.options = dict(config_entry.options)
-
-    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
-        """Manage the options."""
-        return await self.async_step_user()
-
-    async def async_step_user(self, user_input=None):
-        """Handle a flow initialized by the user."""
-        if user_input is not None:
-            self.options.update(user_input)
-            return await self._update_options()
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(x, default=self.options.get(x, True)): bool
-                    for x in sorted(PLATFORMS)
-                }
-            ),
-        )
-
-    async def _update_options(self):
-        """Update config entry options."""
-        return self.async_create_entry(
-            title=self.config_entry.data.get(CONF_IP), data=self.options
-        )
+config_entry_flow.register_discovery_flow(DOMAIN, NAME, _async_has_devices)
